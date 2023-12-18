b2a45edc3cda094335ff78a7579ca07d705c5fe2
James Hoare <james.hoare@appsbroker.com>
2023-12-18 19:36:46 +0000

    +

--- onyx/main.tf
+++ onyx/main.tf
@@ -1,13 +1,13 @@
-module "folder" {
-  source = "../modules/folder"
-  parent = "folders/xxxxx"
-  name   = "Onyx subfolder 1"
-  iam    = jsondecode(file("${path.module}/iam.json"))
-}
+# module "folder" {
+#   source = "../modules/folder"
+#   parent = "folders/xxxxx"
+#   name   = "Onyx subfolder 1"
+#   iam    = jsondecode(file("${path.module}/iam.json"))
+# }
 
-module "folder_2" {
-  source = "./modules/folder"
-  parent = "folders/xxxxx"
-  name   = "Onyx subfolder 2"
-  iam    = jsondecode(file("${path.module}/iam.json"))
-}
+# module "folder_2" {
+#   source = "./modules/folder"
+#   parent = "folders/xxxxx"
+#   name   = "Onyx subfolder 2"
+#   iam    = jsondecode(file("${path.module}/iam.json"))
+# }
