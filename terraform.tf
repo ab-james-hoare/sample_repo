47f0f34b6abb932547af8243fa3dff270b737b56
James Hoare <james.hoare@appsbroker.com>
2023-12-18 17:26:54 +0000

    tweaks

---
+++ terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 0.13.0" #Change me to be more specific once we lock a version down
+  backend "gcs" {
+    bucket = "ab-james-dev-folder-automation-tf-state"
+    prefix = "terraform"
+  }
+}
+
+# Any Google provider specific default configuration can go here
+provider "google" {}
