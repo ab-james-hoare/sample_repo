458f505052aa095eeb84d745fa1214f3977b0593
James Hoare <james.hoare@appsbroker.com>
2023-12-18 22:50:25 +0000

    +

--- biopharmacloud/main.tf
+++ biopharmacloud/main.tf
@@ -4,3 +4,10 @@ module "folder" {
   name   = "Onyx"
   iam    = jsondecode(file("${path.module}/iam.json"))
 }
+
+module "folder_2" {
+  source = "../modules/folder"
+  parent = "folders/621837133107"
+  name   = "Onyx_2"
+  iam    = jsondecode(file("${path.module}/iam.json"))
+}
