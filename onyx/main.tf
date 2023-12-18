b897076caaba35dc236e92744fed50061da5fd38
James Hoare <james.hoare@appsbroker.com>
2023-12-18 21:01:35 +0000

    +

--- onyx/main.tf
+++ onyx/main.tf
@@ -6,7 +6,7 @@ module "folder" {
 }
 
 module "folder_2" {
-  source = "./modules/folder"
+  source = "../modules/folder"
   parent = "folders/897702144504"
   name   = "Onyx subfolder 2"
   iam    = jsondecode(file("${path.module}/iam.json"))
