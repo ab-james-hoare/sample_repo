49f2cba053094fbfc0d4d6a12cb10ecb912e57c6
James Hoare <james.hoare@appsbroker.com>
2023-12-18 17:13:06 +0000

    tf fmt

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "folder" {
   source = "./fabric/modules/folder"
   parent = "folders/621837133107"
   name   = "james auto folder"
-  iam = jsondecode(file("${path.module}/iam.json"))
+  iam    = jsondecode(file("${path.module}/iam.json"))
 
-#   iam = {
-#     "roles/owner" = ["serviceAccount:${var.service_account.email}"]
-#   }
+  #   iam = {
+  #     "roles/owner" = ["serviceAccount:${var.service_account.email}"]
+  #   }
 }
